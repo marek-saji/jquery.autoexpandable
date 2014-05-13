12557d7b741b5dd95182d246988796c26ee709d3
Marek 'saji' Augustynowicz <marek.aug@gmail.com>
2014-05-13 11:39:40 +0200

    Avoid inline anonymouse functions

--- jquery.autoexpandable.js
+++ jquery.autoexpandable.js
@@ -64,6 +64,107 @@
         return transitionDurationProperty;
     }
 
+    // == determinig 1em ==
+    function determineOneEm ($context)
+    {
+        var $foo, oneEm;
+
+        // To determine how much px 1em has, we create
+        // dummy element with some of css attributes cloned.
+        $foo = $('<div />', {
+            'text' : 'Mj',
+            'css' : {
+                'white-space' : 'pre',
+                'font-family' : $context.css('font-family'),
+                'font-size'   : $context.css('font-size'),
+                'line-height' : $context.css('line-height')
+            }
+        });
+        $foo.insertAfter($context);
+        oneEm = $foo.outerHeight();
+        $foo.remove();
+
+        return oneEm;
+    }
+
+    function eventHandler (event)
+    {
+        var $this = $(event.target),
+            options = event.data;
+
+        if (options.timeoutID)
+        {
+            return;
+        }
+
+        // ==== setTimeout ===
+        // Modyfying textarea height after `options.timeout`.
+        options.timeoutID = window.setTimeout(
+            function () {
+                options.timeoutID = null;
+                fit($this, options);
+            },
+            options.timeout
+        );
+    }
+
+    // == fit textarea's height to it's content ==
+    function fit ($this, options)
+    {
+        var transitionDurationProperty = getTransitionDurationProperty($this),
+            transitionDuration = $this.css(transitionDurationProperty),
+            oldHeight,
+            height;
+
+        if ('0s' !== transitionDuration)
+        {
+            // disable transitions so that setting height(0)
+            // allows to read scrollHeight immediatelly
+            $this.css(transitionDurationProperty, '0s');
+        }
+
+        if (! options.paddingBottom)
+        {
+            options.paddingBottom = determineOneEm($this);
+        }
+
+        if (options.first)
+        {
+            if (options.minHeight === undefined)
+            {
+                options.minHeight = $this.height();
+            }
+            else
+            {
+                options.minHeight = Math.max(
+                    options.minHeight,
+                    options.paddingBottom
+                );
+            }
+
+            $this.css('min-height', options.minHeight);
+        }
+
+        if ('' === $this.val())
+        {
+            height = options.minHeight;
+        }
+        else
+        {
+            oldHeight = $this.height();
+            $this.height(0); // to get proper scrollHeight
+            height = $this[0].scrollHeight;
+            height += options.paddingBottom * options.paddingBottomLines;
+            $this.height(oldHeight);
+            $this[0].scrollHeight; // recalculate layout
+        }
+        $this.css(transitionDurationProperty, options.transitionDuration);
+        $this.height(height);
+        $this.css(transitionDurationProperty, transitionDuration);
+
+        options.first = false;
+    }
+
     // == {{{autoexpandable}}} ==
     // function called with {{{$(selector).autoexpandable()}}},
     // {{{this}}} is a set of matched elements
@@ -80,82 +181,22 @@
                 data = $this.data('autoexpandable'),
                 settings = {};
 
-            data || $this.data('autoexpandable', {});
+            if (! data)
+            {
+                data = {};
+                $this.data('autoexpandable', data);
+            }
 
             // merge-in settings specified in `data-autoexpandable` attribute
-            $.extend(settings, invoke_settings, data);
+            data = $.extend(settings, invoke_settings, data);
 
-            $this.bind('keypress.autoexpandable keyup.autoexpandable', function(e){
+            data.first = true;
 
-                var $this = $(this),
-                    data = $this.data('autoexpandable');
-
-                if (data.timeoutID)
-                    return;
-
-                // ==== setTimeout ===
-                // Modyfying textarea height after `settings.timeout`.
-                data.timeoutID = window.setTimeout(function(){
-
-                    data.timeoutID = null;
-
-                    if (settings.minHeight === undefined)
-                    {
-                        settings.minHeight = $this.height();
-                    }
-
-                    if (!settings.paddingBottom)
-                    {
-                        // ==== determinig 1em ====
-                        // To determine how much px 1em has, we create
-                        // dummy element with some of css attributes cloned.
-                        var $foo = $('<div />', {
-                            'text' : 'Mj',
-                            'css' : {
-                                'white-space' : 'pre',
-                                'font-family' : $this.css('font-family'),
-                                'font-size'   : $this.css('font-size'),
-                                'line-height' : $this.css('line-height')
-                            }
-                        });
-                        $foo.insertAfter($this);
-                        data.paddingBottom = $foo.outerHeight();
-                        $foo.remove();
-                    }
-
-                    var padding = data.paddingBottom,
-                        transitionDurationProperty = getTransitionDurationProperty($this),
-                        transitionDuration = $this.css(transitionDurationProperty),
-                        oldHeight,
-                        height;
-
-                    if ('0s' !== transitionDuration)
-                    {
-                        // disable transitions so that setting height(0)
-                        // allows to read scrollHeight immediatelly
-                        $this.css(transitionDurationProperty, '0s');
-                    }
-
-                    if ('' === $this.val())
-                    {
-                        height = padding;
-                    }
-                    else
-                    {
-                        oldHeight = $this.height();
-                        $this.height(0); // to get proper scrollHeight
-                        height = $this[0].scrollHeight;
-                        height += padding * settings.paddingBottomLines;
-                        $this.height( oldHeight );
-                        $this[0].scrollHeight; // recalculate layout
-                    }
-                    $this.css(transitionDurationProperty, settings.transitionDuration);
-                    $this.height(Math.max(settings.minHeight, height));
-                    $this.css(transitionDurationProperty, transitionDuration);
-
-                }, settings.timeout); // setTimeout
-
-            }).trigger('keypress.autoexpandable');
+            $this.on(
+                'keypress.autoexpandable keyup.autoexpandable',
+                data,
+                eventHandler
+            ).trigger('keypress.autoexpandable');
 
         }); // return each this
 
